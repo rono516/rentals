4f4669225c12a890ac45544d5490f73ce66d8ec9
Collins Kiplangat Rono <ronocollins2000@gmail.com>
2024-10-27 12:45:04 +0300

    feature/no-ref/create-tenant-account-through-email

--- routes/web.php
+++ routes/web.php
@@ -11,17 +11,6 @@ use App\Http\Controllers\TenantController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/mailable', function () {
     $tenant = App\Models\Tenant::find(1);
 
